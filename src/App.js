49f05461c6d76877b77a99a3ed041375de20255d
Sergius92739 <sergem279@gmail.com>
2022-07-15 16:46:31 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CountingWorkouts from "./components/CountingWorkouts/CountingWorkouts";
+import Form from "./components/Form/Form";
+
+
+function App() {
+  return (
+    <CountingWorkouts />
+  )
+}
+
+export default App;
